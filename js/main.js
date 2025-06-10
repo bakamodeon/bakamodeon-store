69052fa810ab98cebcbcbfa00d589450dc1e64e9
Mrinmay Basu <mrinmay.basu.780@gmail.com>
2025-06-11 00:03:31 +0530

    Initial commit for Baka Mode On

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Smooth scroll (if using # links)
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+    });
+  });
